a01bb4ee8c582850848e82071c9ad96607c02d21
vitao04 <110508972+vitao04@users.noreply.github.com>
2022-09-05 20:38:09 -0300

    Update TesteCliente.java
    Criando a classe cliente

--- TesteCliente.java
+++ TesteCliente.java
@@ -3,5 +3,22 @@ public class TesteCliente{
 
     System.out.println("Processando o cliente...")
 }
+ Class Cliente{
+  String nome;
+  String cpf;
+  public void setNome(String nome){
+  this.nome = nome;
+  }
+  
+public String getNome(){
+this.nome
+ }
+  public String setCpf(String nome){
+  this.nome
+   }
+  public String getCpf(){
+  this.cpf
+  }
+ }
 
 }
